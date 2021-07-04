a93f879f8034f198012686a1459095f1f0815f77
Daniel Wang <haidixiansheng@126.com>
2021-07-04 09:18:10 +0800

    Kunkka 1.22
    原版

---
+++ sidebar.php
@@ -0,0 +1,5 @@
+<div id="sidebar">
+    <?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'sidebar' ) ) : ?>
+        <p><?php _e( 'Please set up widgets at dashboard!', MUTHEME_NAME ); ?></p>
+    <?php endif; ?>
+</div>
