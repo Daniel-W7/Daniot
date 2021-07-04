8b10954b36c7700071971348933c43aa6ff6a798
Daniel Wang <haidixiansheng@126.com>
2021-07-04 12:38:23 +0800

    V1.22
    神韵修改版

--- search.php
+++ search.php
@@ -1,27 +1,5 @@
 <?php get_header(); ?>
-
-<!-- bing搜索配置 -->
-<form method="get" class="search-form" action="https://cn.bing.com/?ensearch=1&FORM=BEHPTB">
-      <input type="text" class="search-input" id="bing" placeholder="请输入bing搜索关键词"   onkeydown="if(event.keyCode==13){return Search('bing');}"/>
-	   	    <button type="submit" class="search-submit" onClick="return Search('bing');">
-				<span class="mutheme-search"></span>
-			</button>
-		</form>	
-		<script type="text/javascript">
-		function Search(type){
-   		 if (type=="bing") {
-        		var value=$("#bing").val()||"";
-       			 window.open("https://cn.bing.com/search?q="+value+"&qs=n&form=QBLHCN&sp=-1&pq=12&sc=9-2&sk=&cvid=5D551A28C93A4D39AFB42949C4AB8101"); 
-   		 } 
-		 else {
-        	var value=$("#wp_search").val()||"";
-        	window.open("https://zhang.ge/search/"+value+"/"); 
-   			 }
-    	return false;
-		}
-</script>
-<!-- 站内搜索配置 -->
-<div id="primary">
+    <div id="primary">
         <div class="breadcrumb-navigation">
             <a rel="bookmark" href="<?php echo home_url(); ?>"><?php _e( 'Home', MUTHEME_NAME ); ?></a>
             <span class="breadcrumb-arrow"></span><?php _e( 'Search keyword', MUTHEME_NAME ); ?>
@@ -94,6 +72,5 @@
             <?php mutheme_pagenavi(); ?>
         </div>
     </div>
-	
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
