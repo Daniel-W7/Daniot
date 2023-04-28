ab2fb9cca54c75a9d9550d68b69bdcc838b3d279
Daniel Wang <wanghaidi7@gmail.com>
2023-04-28 18:04:06 +0800

    进行新的模板开发，保留之前的开发记录，以作参考

--- header.php
+++ header.php
@@ -2,32 +2,9 @@
 <!--定义网站标题-->
 <div id="header">
     <div class="header">
-        <!--根据是不是主页配置不同的类-->
-        <?php
-            if (is_home()) {
-            echo '<h1 class="logo">';
-            }else{
-            echo '<h2 class="logo">';
-            }
-        ?>
-        <!--顶部显示网站名称，logo-->
         <a href="<?php bloginfo( 'url' ); ?>" title="<?php bloginfo( 'name' ); ?>" rel="home">
-        <?php bloginfo( 'name' ); ?>
-        <!--
-        <img src="<?php //echo daniot_image( 'logo.png' ); ?>" 
-                alt="<?php //bloginfo( 'name' ); ?>" 
-                width="102"
-                height="30"/>
-        -->
+            <?php bloginfo( 'name' ); ?>
         </a>
-        <!--根据是不是主页配置显示内容，使首页标题横向显示-->
-        <?php
-        if (is_home()) {
-            echo '</h1>';
-        }else{
-            echo '</h2>';
-        }
-        ?>
         <div class="global-nav">
             <?php wp_nav_menu( array(
                 'theme_location'  => 'top-menu',
