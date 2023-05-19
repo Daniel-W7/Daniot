351bf399fdec3b8d573ab231ea03c4b0eea51b94
Daniel Wang <wanghaidi7@gmail.com>
2023-05-19 16:09:23 +0800

    配置404页面

--- 404.php
+++ 404.php
@@ -1,41 +1,13 @@
-<?php
-/**
- * The template for displaying 404 pages (Not Found)
- *
- */
-get_header(); ?>
-    <div id="primary" class="full-page">
-        <div class="errorDiv">
-            <a href="<?php bloginfo( 'url' ); ?>" title="<?php bloginfo( 'name' ); ?>" rel="home">
-                <img src="<?php echo daniot_image( '404.png' ); ?>" alt="<?php bloginfo( 'name' ); ?>" width="55%"; />
-            </a>
-            <p><h1>Not Found</h1></p>
-            <p>对不起，您访问的界面已迁移或不存在，即将返回首页</p>
-            <p><span id="timedown" style="font-size:2em;color:#EB4444"></span> s </p>
-        </div>
-    </div>
-    <!-- #primary -->
-    <style>
-    .errorDiv{
-        margin: 0 auto;
-        text-align: center;
-    }
-    </style>
-    <script type="text/javascript">  
-    //设定倒数秒数  
-    var t = 15;  
-    //显示倒数秒数  
-    function showTime(){  
-        t -= 1;  
-        document.getElementById('timedown').innerHTML= t;  
-        if(t==0){  
-            location.href='/';  
-        }  
-        //每秒执行一次,showTime()  
-        setTimeout("showTime()",1000);  
-    }  
-
-    //执行showTime()  
-    showTime();  
-    </script>  
-<?php get_footer(); ?>
+<?php get_header() ?>
+<!--错误页面-->
+<table class="base">
+    <div class="block">
+      <tr>
+        <td style="text-align: center; padding-right: 10px; letter-spacing: 1px; vertical-align: top;">您想访问的页面并没有找到，您可以尝试右上角搜索或者查看其它相关文章-。-</td>
+      </tr>
+      <tr>
+        <td style="width: 10px; text-align: center; vertical-align: top;">祝您今天万事顺利0.0</td>
+      </tr>
+    </div><!-- block -->			
+</table><!--base-->
+<?php get_footer() ?>
